ed17a533af5c65291ebef21450a3935dc8a306ef
Cod3ingKing <crb565@gmail.com>
2022-02-17 00:15:47 -0500

    project comp

--- js/script.js
+++ js/script.js
@@ -3,30 +3,22 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance:
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/***
- * `quotes` array
-***/
+//my quotes
 const quotes = [
   {quote: `${"Never follow anyone else's path. Unless you're in the woods and you're lost and you see a path. Then by all means follow that path."}`,source:' Ellen DeGeneres'},
   {quote: `${"I only have a minute; only sixty seconds in it; didin't seek it, didin't choose it, but it's up to me to use it. Just a tiny little minute... but eternity is in it."}`,source:' Benjamin Elijah Mays'},
   {quote: `${"Every man and woman is born into the world to do something unique and something distinctive and if he or she does not do it, it will never be done."}`,source:' Benjamin Elijah Mays'},
-  {quote: `${"One can have no smaller or greater mastery than mastery of oneself."}`,source:' Leonardo da Vinci',year:' 1993',tags:' History'},
+  {quote: `${"One can have no smaller or greater mastery than mastery of oneself."}`,source:' Leonardo da Vinci',year:' 1500',tags:' History'},
   {quote: `${"Eloquence is a painting of the thoughts"}`,source:' Blaise Pascal',citation:' the house of love', tags:' Art'},
-  {quote: `${"I want to learn all those words so I can insult people without them knowing."}`,source:' Sam Burns',citation:' the house of love'}
+  {quote: `${"I want to learn all those words so I can insult people without them knowing."}`,source:' Sam Burns',citation:' 22 Home'}
 
 ];
-
+//background color array. Not fully finished asked for help on slack Unit 1,Feb 6 2022 8:14pm
 const colorArray = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+//
 
-
-
-/***
- * `getRandomQuote` function
-***/
+//getRandomQuote function to generate a random number and call a string from the array. C-III
 
 function getRandomQuote(){
   const randomNumber = Math.floor (Math.random() * quotes.length);
@@ -34,42 +26,34 @@ function getRandomQuote(){
   return (quotes[(randomNumber)]);
 }
 
+//selects a random number from the colorArray
+function getRandomColor(){
+  const randomNumber = Math.floor (Math.random() * colorArray.length);
 
+  return (colorArray[(randomNumber)]);
+}
+// backgroundColor code. still working on it. each iteration of the loop should pick a random number between 0 and 15 and then grab it from colorArray[randomNumber]
 function getRandomHexValue(){
 
+  const randColor = getRandomColor();
   let color = '#';
   for (let i=0; i<6; i++) {
-      color += getRandomHexValue();
-  }
-element.styles.backgroundColor = getRandomHexValue([(randomValue)]);
-  // document.body.style.backgroundColor = 'rgb(' + Math.round (Math.random() *255)';
+      color += randColor;
 
+
+  }
+// should print a random color on page (not finished)
+  const body = document.querySelector('body');
+
+  body.styles.backgroundColor = getRandomHexValue();
 
 }
-// console.log (getRandomQuote().quote);
+//prints a random quote on page for each click
 
-// let random =
-// let quote = a[random];
-
-// for (let i = 0;i > quotes.length; i++){
-//
-// let quote = quotes[i].quote;
-// let source = quotes[i].source;
-
-
-
-// console.log (quotes[getRandomQuote ()].quote );
-// for (let i = 0;i < quotes.length; i++)
-
-
-
-/***
- * `printQuote` function
-***/
 function printQuote(){
   const randQuote = getRandomQuote();
 
-
+//omits quote object propertys that are undifined
   let htmlString;
   htmlString += `<p class="quote"> ${randQuote.quote} </p>`;
   htmlString += `<p class = "source"> ${randQuote.source}`;
@@ -85,32 +69,12 @@ function printQuote(){
   htmlString += `</p>`;
 
 
-
+//prints html string on page
 
 document.getElementById('quote-box').innerHTML = htmlString;
 }
 
 
-
-// ;
-// const randSource = quotes[getRandomQuote ()].source
-
-// const htmlQuote = 'hello';
-
-// <p class = "quote"> `${quotes[getRandomQuote ()].quote}` </p>
-// <p class = "source"> `${quotes[getRandomQuote ()].source}`
-//
-//
-//
-// </p>
-
-
-
-
-
-
-
-// document.getElementById('quote-box').innerHTML = html;
 
 /***
  * click event listener for the print quote button
